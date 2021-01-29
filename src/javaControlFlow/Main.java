7f16ffcc763e5720565eb5f6ab8e91d8f3c0014f
Joey Whalen <76399957+joeywhalen@users.noreply.github.com>
2021-01-29 16:40:43 -0500

    Add files via upload

--- src/javaControlFlow/Main.java
+++ src/javaControlFlow/Main.java
@@ -1,10 +1,34 @@
 package javaControlFlow;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		System.out.println("Numbers:");// print the valid numbers from AsciiChar file
+		AsciiChars.printNumbers();
 
+		System.out.println("Lowercase letters:");
+		AsciiChars.printLowerCase();// print the valid letters from AsciiChar file
+		
+		System.out.println("Uppercase letters:");
+		AsciiChars.printUpperCase();// print the valid letters from AsciiChar file
+
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Please enter your name:");//request and capture user name
+		String inputName = userInput.nextLine();
+		System.out.printf("Hello %s\n", inputName);
+		
+		System.out.println("Do you wish to continue to the interactive portion? (y/n)");// determine if program ends or continues
+		String userResponse = userInput.nextLine();
+		
+		if(userResponse.equalsIgnoreCase("y") || userResponse.equalsIgnoreCase("yes")) {
+			System.out.println("What is the name of your favorite pet?");// request information to convert to lottery numbers
+			String favPet = userInput.nextLine();
+			
+			
+		}
 	}
 
 }
