04eebc2780b73c015514fa13f1b3f9177d50e904
Joey Whalen <76399957+joeywhalen@users.noreply.github.com>
2021-01-29 15:40:19 -0500

    Add files via upload

---
+++ src/javaControlFlow/AsciiChars.java
@@ -0,0 +1,35 @@
+package javaControlFlow;
+
+public class AsciiChars {
+
+	public static void printNumbers() {
+	    // TODO: print valid numeric input
+		// ASCii numbers range = 48 to 57
+		for(int n = 48; n < 58; n++) {
+			System.out.printf("%c, ", n);
+		}
+		System.out.printf("\n");
+		// adds a new line
+	  }
+
+	  public static void printLowerCase() {
+	    // TODO: print valid lowercase alphabetic input
+		// ASCii numbers range = 97 to 122
+		for(int l = 97; l < 123; l++) {
+			System.out.printf("%c, ", l);
+		}
+		System.out.printf("\n");
+		// adds a new line
+	  }
+
+	  public static void printUpperCase() {
+	    // TODO: print valid uppercase alphabetic input
+		// ASCii numbers range = 65 to 90
+		for(int u = 65; u < 91; u++) {
+			System.out.printf("%c ", u);
+		}
+		System.out.printf("\n");
+		// adds a new line
+	}
+
+}
